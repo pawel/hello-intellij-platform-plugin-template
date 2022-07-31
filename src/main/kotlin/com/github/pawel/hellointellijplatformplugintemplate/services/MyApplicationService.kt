68566bd41421a9fac8e5d4ba1f849944a2bcf03b
GitHub Action <action@github.com>
2022-07-31 09:39:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pawel/hellointellijplatformplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pawel.hellointellijplatformplugintemplate.services
+
+import com.github.pawel.hellointellijplatformplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/pawel/hellointellijplatformplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pawel/hellointellijplatformplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pawel.hellointellijplatformplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pawel.hellointellijplatformplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
